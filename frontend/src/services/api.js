72d669038480ff84afb4190285d5030d451fb7f5
developer-xemail <kanhaiygupta991018@gmail.com>
2026-04-09 19:35:09 +0530

    fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://13.232.192.246:8080/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://52.66.201.255:8080/api',
   timeout: 10000,
 })
 
